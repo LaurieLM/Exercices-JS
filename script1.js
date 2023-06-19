a1d22e50546969f61e054f9f9d0ca632abf98ca8
LESPINASSE MARINI <lespinasse.laurie5@gmail.com>
2023-06-19 10:09:30 +0200

    error2

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
-let chiffre1 = 5;
-let chiffre2 = 2;
+let chiffre1 = 10;
+let chiffre2 = 1;
 let button_addition = document.getElementById('button_addition')
 
 // console.log(chiffre1);
@@ -9,15 +9,23 @@ if (chiffre1 < 0 || chiffre1 > 9 || chiffre2 < 0 || chiffre2 > 9) {
     document.getElementById('message_error1').style.visibility = "visible";
 };
 
-
+let resultat = chiffre1 + chiffre2;
+// console.log(resultat);
+let resultat_elem = document.createElement('p')
+let resultat_nombre = document.createTextNode(resultat);
+let resultat_affichage = document.getElementById('resultat_affichage');
+    
 button_addition.addEventListener('click', event => {
-    let resultat = chiffre1 + chiffre2;
-    console.log(resultat);
-    let resultat_elem = document.createElement('p')
-    let resultat_nombre = document.createTextNode(resultat);
-    let resultat_affichage = document.getElementById('resultat_affichage');
-    resultat_affichage.appendChild(resultat_elem);
-    resultat_elem.appendChild(resultat_nombre);
-
+    if (typeof chiffre1 !== 'number' || typeof chiffre2 !== 'number' || chiffre1 == null || chiffre2 == null) {
+        document.getElementById('message_error2').style.visibility = "visible";
+    
+    } else {
+        resultat_affichage.appendChild(resultat_elem);
+        resultat_elem.appendChild(resultat_nombre);
+    };   
 });
     
+// if (chiffre1 = null || chiffre1 < 0 || chiffre1 > 9 || chiffre2 < 0 || chiffre2 > 9) {
+//     document.getElementById('message_error2').style.visibility = "visible";
+
+// };
